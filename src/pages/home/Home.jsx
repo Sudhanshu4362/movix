bd15ffc45987b979af002ec828fdd76b0166153c
Sudhanshu Mishra <mishrasudhanshu855@gmail.com>
2023-09-04 12:33:45 +0530

    Home page completed details started

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./style.scss";
+
+import HeroBanner from './HeroBanner/HeroBanner';
+import Trending from './trending/Trending';
+import Popular from './popular/Popular';
+import TopRated from './topRated/Toprated';
+const Home = () => {
+  return (
+    <div>
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+        </div>
+    </div>
+  )
+}
+
+export default Home
